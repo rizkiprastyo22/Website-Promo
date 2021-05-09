a4b5e4abeb4ec6b1a40c704f71d20209a74bb3b0
Rizki Prastyo <rizkiprastyo22@gmail.com>
2021-05-09 15:56:06 +0700

    commit now

--- application/views/dashboard/main.php
+++ application/views/dashboard/main.php
@@ -1,3 +1,29 @@
+<!-- <h1>Ini fungsi index dari controller dashboard</h1>
 <h1>Ini fungsi index dari controller dashboard</h1>
-<h1>Ini fungsi index dari controller dashboard</h1>
-<h1>Ini fungsi index dari controller dashboard</h1>
+<h1>Ini fungsi index dari controller dashboard</h1> -->
+
+<div class="container">
+	<!-- Top box -->
+		<!-- Logo & Site Name -->
+		<div class="placeholder">
+			<div class="parallax-window" data-parallax="scroll" data-image-src="img/bg1.png">
+				<div class="tm-header">
+					<div class="row tm-header-inner">
+						<div class="col-md-6 col-12">
+							<img src="img/logoku.png" alt="Logo" class="tm-site-logo" width="200" 
+							height="50" /> <br>
+							<div class="tm-site-text-box">
+								<h6 class="tm-site-description" style="color: black; padding-left: 5px;">Segala Promo, Ada di Hemat Warrior!</h6>	
+							</div>
+						</div>
+						<!-- <nav class="col-md-6 col-12 tm-nav">
+							<ul class="tm-nav-ul">
+								<li class="tm-nav-li"><a href="index.html" class="tm-nav-link active">Home</a></li>
+								<li class="tm-nav-li"><a href="about.html" class="tm-nav-link">About</a></li>
+								<li class="tm-nav-li"><a href="contact.html" class="tm-nav-link">Contact</a></li>
+							</ul>
+						</nav>	 -->
+					</div>
+				</div>
+			</div>
+		</div>
